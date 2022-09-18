0adcd39d69757cc5aefd93d66e4113a181415570
digitaldiver <1119han@gmail.com>
2022-09-18 14:43:20 +0200

    WIP 21

--- 21-selection-sort.rb
+++ 21-selection-sort.rb
@@ -1,12 +1,18 @@
 # Given an array with numbers, please implement sorting
-# https://en.wikipedia.org/wiki/Selection_sort
+# https://en.wikipedia.org/wiki/Selction_sort
 
-def selection_sort(arr)
-  arr.sort!
+def selction_sort(arr)
+  n = arr.length - 1
+  n.times do |i|
+    min_index = i
+    for j in (i + 1)..n do
+      min_index = j if arr[j] < arr[min_index]
+    end
+  end
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
 
-answer = selection_sort(arr)
+answer = selction_sort(arr)
 
 puts answer.to_s # The result should be [1, 7, 9, 42, 46, 46, 68, 77, 86, 91]
